f9c77cf7237186b0668370a39068e24a5a4c1a80
artem <iakonien@gmail.com>
2025-03-27 16:44:58 +0300

    Initial commit

---
+++ src/main/java/com/artemyakkonen/spring/boot/ttmicroservice2/TtMicroservice2Application.java
@@ -0,0 +1,13 @@
+package com.artemyakkonen.spring.boot.ttmicroservice2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtMicroservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtMicroservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemyakkonen/spring/boot/ttmicroservice2/TtMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemyakkonen.spring.boot.ttmicroservice2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtMicroservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
