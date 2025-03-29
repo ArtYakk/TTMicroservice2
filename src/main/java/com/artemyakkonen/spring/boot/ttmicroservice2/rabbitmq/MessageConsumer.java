f35ffda3eabadae0a67f7ba58d23d131a1822d19
artem <iakonien@gmail.com>
2025-03-29 14:23:01 +0300

    3

--- src/main/java/com/artemyakkonen/spring/boot/ttmicroservice2/rabbitmq/MessageConsumer.java
+++ src/main/java/com/artemyakkonen/spring/boot/ttmicroservice2/rabbitmq/MessageConsumer.java
@@ -1,8 +1,11 @@
 package com.artemyakkonen.spring.boot.ttmicroservice2.rabbitmq;
 
+import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Component
 @RabbitListener(queues = "myQueue")
@@ -12,4 +15,5 @@ public class MessageConsumer {
     public void receiveMessage(String message) {
         System.out.println("Received \n" + message);
     }
+
 }
